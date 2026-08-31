3cef143e0fbcce1c57ae4dc3fcf4529aafb5fe5b
Martin Nester <martinn7352@gmail.com>
2026-08-31 09:03:28 -0400

    add linked in

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -344,6 +344,8 @@
 
 		{@render slider()}
 	</div>
+	<div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="martinnester" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/martinnester?trk=profile-badge">Martin Nester</a></div>
+              
 {/snippet}
 
 {#snippet unpinAction()}
